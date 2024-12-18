f0c6cf02a7f3d50ebeab20770bd7b7b17b3a86dd
BugHunter-X <askzenai@gmail.com>
2024-12-18 01:38:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,11 @@
+```javascript
+// some tailwind classes that are not working
+<div class="flex items-center justify-between p-4 bg-gray-200 rounded-lg shadow-md">
+  <div class="text-xl font-bold">
+    My Component
+  </div>
+  <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+    Button
+  </button>
+</div>
+```
